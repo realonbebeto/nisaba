9fbfe8d04ab91082be21391e8f0c2f9713d8f6c0
Bebeto Nyamwamu <realonbbt@gmail.com>
2026-01-30 15:00:37 -0500

    chore: all sources supported

---
+++ examples/multiple_sources.rs
@@ -0,0 +1,66 @@
+use nisaba::{
+    AnalyzerConfig, DistanceType, EmbeddingModel, SchemaAnalyzer, ScoringConfig, SimilarityConfig,
+    Source,
+};
+
+#[tokio::main]
+async fn main() {
+    let config = AnalyzerConfig::builder()
+        .sample_size(1000)
+        .scoring(ScoringConfig {
+            type_weight: 0.65,
+            structure_weight: 0.35,
+        })
+        .similarity(SimilarityConfig {
+            threshold: 0.59,
+            top_k: Some(7),
+            algorithm: DistanceType::Cosine,
+        })
+        .build();
+
+    // analyzer
+    let analyzer = SchemaAnalyzer::builder()
+        .name("nisaba")
+        .config(config)
+        .embedding_model(EmbeddingModel::MultilingualE5Small)
+        .source(Source::csv("./assets/csv", None).unwrap())
+        .sources(vec![
+            Source::parquet("./assets/parquet", None).unwrap(),
+            Source::excel("./assets/xlsx", None).unwrap(),
+            Source::mongodb()
+                .auth("mongodb", "mongodb")
+                .host("localhost")
+                .database("mongo_store")
+                .pool_size(5)
+                .port(27017)
+                .build()
+                .unwrap(),
+            Source::mysql()
+                .auth("mysql", "mysql")
+                .host("localhost")
+                .port(3306)
+                .database("mysql_store")
+                .build()
+                .await
+                .unwrap(),
+            Source::postgresql()
+                .auth("postgres", "postgres")
+                .host("localhost")
+                .database("postgres")
+                .port(5432)
+                .namespace("public")
+                .build()
+                .await
+                .unwrap(),
+            Source::sqlite()
+                .path("./assets/sqlite/nisaba.sqlite")
+                .build()
+                .await
+                .unwrap(),
+        ])
+        .build()
+        .await
+        .unwrap();
+
+    let _result = analyzer.analyze().await.unwrap();
+}
